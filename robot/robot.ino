ae2a136f92c4e29b77b5917f322eb7a2962fd30c
Jakub-S-K <jakub.stanula.kaczka@gmail.com>
2022-11-24 21:51:20 +0100

    major refactor chapter 1 part 2

--- robot/robot.ino
+++ robot/robot.ino
@@ -3,53 +3,72 @@
 #define sharp_RS_Pin 12 // PA2
 #define sharp_LS_Pin 13 // PA3
 
-#define ground_RF_Pin 30 //PA9
-#define ground_LF_Pin 32 //PA11
-#define ground_RB_Pin 29 //PA8
-#define ground_LB_Pin 31 //PA10
+#define ground_RF_Pin 30 // PA9
+#define ground_LF_Pin 32 // PA11
+#define ground_RB_Pin 29 // PA8
+#define ground_LB_Pin 31 // PA10
 
-//ENGINE PINOUT
-//ENGINE L albo prawy 
-#define EngineL_PWM 21 //PWM pb10
-#define EngineL_F 20 //Przut gdy wysoki  :3 pb2
-#define EngineL_B 19 //tył gdy wysoki  pb1
+// ENGINE PINOUT
+// ENGINE L albo prawy
+#define EngineL_PWM 21 // PWM pb10
+#define EngineL_F 20   // Przut gdy wysoki  :3 pb2
+#define EngineL_B 19   // tył gdy wysoki  pb1
 //----------------------t -
-//GDY ENGINE F i B są wysokie jednocześnie to jest zwarcie
+// GDY ENGINE F i B są wysokie jednocześnie to jest zwarcie
 //-----------------------
-//ENGINE R albo lewy
-#define EngineR_PWM 18 //PWM pb0
-#define EngineR_F 17 //przut :3 pa7
-#define EngineR_B 16 //tył pa6
+// ENGINE R albo lewy
+#define EngineR_PWM 18 // PWM pb0
+#define EngineR_F 17   // przut :3 pa7
+#define EngineR_B 16   // tył pa6
 
-#define START_PIN 40 //PB4 CHYBA!
+#define ENEMY_TRESHOLD 300
 
-//engines
+#define LEFT_FORWARD (1)
+#define LEFT_BACKWARD (1 << 1)
+#define RIGHT_FORWARD (1 << 2)
+#define RIGHT_BACKWARD (1 << 3)
+#define DIR_STOP (1 << 4)
+
+#define START_PIN 40 // PB4
+#define STOP_PIN 41  // PB5
+
+enum MODE
+{
+  BORDER,
+  SCAN,
+  PUSH
+} logic = MODE::SCAN;
+
+// engines
 bool EngineL = false; // false backward, true forward
 bool EngineR = false; // false backward, true forward
-unsigned short EngineLPWM = 0; 
+unsigned short EngineLPWM = 0;
 unsigned short EngineRPWM = 0;
 
-//SENSORS
+// SENSORS
 
-//ground sensor L=Left R=Right F=Front B=Back
+// ground sensor L=Left R=Right F=Front B=Back
 bool ground_LF = false;
 bool ground_RF = false;
 bool ground_LB = false;
 bool ground_RB = false;
 
-//FRONT SHARP
+bool programStart = true;
+
+// FRONT SHARP
 short sharp_LF = 0;
 short sharp_RF = 0;
-//SIDE SHARP
+// SIDE SHARP
 short sharp_LS = 0;
 short sharp_RS = 0;
 
-inline void Move(short speed, float speedProportion, bool LE_direction, bool RE_direction);
+inline void Move(uint8_t throttle, uint32_t directions);
 inline void MoveAleInaczej(short x, short y);
 inline void readSensors();
 bool setState();
 
-void setup() {
+void setup()
+{
 
   pinMode(START_PIN, INPUT_PULLUP);
   pinMode(sharp_LF_Pin, INPUT);
@@ -59,16 +78,29 @@ void setup() {
 
 #ifdef DEBUG
   delay(100);
-  Serial.begin(115200);  
+  Serial.begin(115200);
 #endif
 }
 
-void loop() {
+void loop()
+{
 
-  while(digitalRead(START_PIN)); //Wait for LOW voltage to start
+  if (programStart)
+  {
+    programStart = !digitalRead(START_PIN);
+    return;
+  }
+  if (digitalRead(STOP_PIN)) // TODO: fix condition statement
+  {
+    while (true)
+      ;
+  }
+
   readSensors();
-  if(setState()) { //setState return true on abrupt change needing action immediately
-    return; 
+
+  if (setState())
+  { // setState return true on abrupt change needing action immediately
+    return;
   }
 
 #ifdef DEBUG
@@ -78,28 +110,62 @@ void loop() {
   Serial.print(',');
   Serial.print(sharp_RF + sharp_LF);
   Serial.print(',');
-  Serial.println(sharp_LF - sharp_RF);  
+  Serial.println(sharp_LF - sharp_RF);
 #endif
-  
 }
 
-inline void readSensors()  { 
+inline void readSensors()
+{
   sharp_LF = analogRead(sharp_LF_Pin);
   sharp_RF = analogRead(sharp_RF_Pin);
   sharp_LS = analogRead(sharp_LS_Pin);
   sharp_RS = analogRead(sharp_RS_Pin);
 }
 
-void Move(short speed, float speedProportion, bool LE_direction, bool RE_direction){ //s
-  //speed 
-  //bananas rotate ex 90deg clockwise -90deg counterclockwise 
+inline void Move(uint8_t throttle, uint32_t directions)
+{
+  if (directions & DIR_STOP)
+  {
+    digitalWrite(EngineL_B, LOW);
+    digitalWrite(EngineL_F, LOW);
+    digitalWrite(EngineR_B, LOW);
+    digitalWrite(EngineR_F, LOW);
+    return;
+  }
+  if (directions & LEFT_FORWARD)
+  {
+    digitalWrite(EngineL_B, LOW);
+    digitalWrite(EngineL_F, HIGH);
+  }
+  else if (directions & LEFT_BACKWARD)
+  {
+    digitalWrite(EngineL_F, LOW);
+    digitalWrite(EngineL_B, HIGH);
+  }
+
+  if (directions & RIGHT_FORWARD)
+  {
+    digitalWrite(EngineR_B, LOW);
+    digitalWrite(EngineR_F, HIGH);
+  }
+  else if (directions & RIGHT_BACKWARD)
+  {
+    digitalWrite(EngineR_F, LOW);
+    digitalWrite(EngineR_B, HIGH);
+  }
 }
 
-void MoveAleInaczej(short x, short y){
-  //niewiem
-}
+bool setState()
+{ // Set state machine based on sensor input
+  switch (logic)
+  {
+  case MODE::BORDER:
+    /* code */
+    break;
 
-bool setState() { // Set state machine based on sensor input
+  default:
+    break;
+  }
 
   return true;
 }
